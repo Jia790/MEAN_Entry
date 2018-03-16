ec87550df8db891a3a573770c900cf3e13c161db
Jia790 <jia790@gmail.com>
2018-03-16 15:05:32 -0700

    added everyting for heroku deloyment

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
     // uncommon the following if using own database
     // database: 'mongodb://localhost:27017/meanAuth',
-    database: 'mongodb://localhost:27017/meanAuth', // mlab db link
+    database: 'mongodb://jia790:Young1110@ds217349.mlab.com:17349/mean_quote', // mlab db link
     secret: 'meansecret'
 }
